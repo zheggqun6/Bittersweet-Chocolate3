a7ab72ce71d4179a7ff6486b4692f4a0242b8242
zheggqun6 <xingranr15955@163.com>
2021-04-06 23:20:23 +0800

    feat: 模板编译添加

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@
  * @Author: zihao.chen
  * @Date: 2020-09-18 16:02:00
  * @LastEditors: zihao.chen
- * @LastEditTime: 2020-12-22 16:24:15
+ * @LastEditTime: 2020-12-25 09:50:56
  * @Description: vue 初始化
  */
 
@@ -16,5 +16,21 @@ export function initMixin(Vue) {
     // 初始化状态 （初始化劫持数据，改变数据时更新视图）
     // 添加状态
     initState(vm)
+    
+    // 如果当前有el属性说明要渲染模板
+    if(vm.$options.el){
+        vm.$mount(vm.$options.el)
+    }
+  }
+  Vue.prototype.$mount = function (el) { 
+    // 挂载操作
+    const vm = this;
+    const vm = this;
+    el = document.querySelector(el)
+    console.log(el)
+
+    if(vm.$options.render){
+      
+    }
   }
 }
