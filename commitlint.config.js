d956559b4a9d83d40209908e13caac2e6b6afe78
zheggqun6 <xingranr15955@163.com>
2021-04-17 19:54:03 +0800

    fix: 修改配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,28 +2,28 @@
  * @Author: zihao.chen
  * @Date: 2021-02-19 10:52:39
  * @LastEditors: zihao.chen
- * @LastEditTime: 2021-02-19 10:53:12
+ * @LastEditTime: 2021-02-19 11:09:30
  * @Description: 提交配置文件
  */
-
- // build：主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交或打前端包
-// docs：文档更新
-// feat：新增功能
-// merge：分支合并 Merge branch ? of ?
-// fix：bug 修复
-// perf：性能, 体验优化
-// refactor：重构代码(既没有新增功能，也没有修复 bug)
-// style：不影响程序逻辑的代码修改(修改空白字符，格式缩进，补全缺失的分号等，没有改变代码逻辑)
-// test：新增测试用例或是更新现有测试
-// revert：回滚某个更早之前的提交
-// chore：不属于以上类型的其他类型
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', [
-      "build", "docs", "feat", "merge", "fix", "perf", "refactor", "style", "test", "revert", "chore"
-    ]],
+    'type-enum': [
+      2,
+      'always',
+      [
+      'feat', // 新功能（feature）
+      'fix', // 修补bug
+      'docs', // 文档（documentation）
+      'style', // 格式（不影响代码运行的变动）
+      'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+      'test', // 增加测试
+      'revert', // 回滚
+      'config', // 构建过程或辅助工具的变动
+      'chore', // 其他改动
+      ],
+    ],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never']
   }
